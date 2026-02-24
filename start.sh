fbee9e9bea8c136f30b09044c1796d3464e30c95
Subojeet448 <mandalsubojeet8@gmail.com>
2026-02-24 18:14:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Make sure script is executable: chmod +x start.sh
-
 echo "Starting FastAPI server..."
-# Use Render's PORT environment variable
-uvicorn app:app --host 0.0.0.0 --port $PORT
+uvicorn app:app --host 0.0.0.0 --port ${PORT:-8000}
