fde076df69c0d809c88c25f7492b023c6e40db68
Subojeet448 <mandalsubojeet8@gmail.com>
2026-02-25 07:22:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Make sure the file has execute permission: chmod +x start.sh
-
-# Run FastAPI app using Render's PORT
-uvicorn app:app --host 0.0.0.0 --port $PORT
+echo "Starting FastAPI TTS server..."
+exec uvicorn app:app --host 0.0.0.0 --port $PORT
